cb77d85f8a2d6c7ede142b5c2c4cca661d9b967b
Shamil <kasum543@gmail.com>
2025-01-04 15:58:51 +0300

    create file with todo tasks

--- 01-todo-list/internal/storage/storage.go
+++ 01-todo-list/internal/storage/storage.go
@@ -1,5 +1,13 @@
 package storage
 
+import (
+	"errors"
+	"log"
+	"os"
+)
+
+const path = "./input.csv"
+
 type Storage interface {
 	Add(string) int
 	Delete()
@@ -8,12 +16,32 @@ type Storage interface {
 }
 
 type storage struct {
+	path string
+}
+
+func (s *storage) getMaxID() int {
+	// TODO генерировать инкремент ID
+	return 0
 }
 
 var s *storage
 
 func init() {
-	s = &storage{}
+	s = &storage{
+		path: path,
+	}
+
+	if _, err := os.Stat(s.path); errors.Is(err, os.ErrNotExist) {
+		fo, err := os.Create(s.path)
+		if err != nil {
+			log.Fatalf("failed to create file: %s", err)
+		}
+		defer func() {
+			if err := fo.Close(); err != nil {
+				log.Fatalf("failed to close file: %s", err)
+			}
+		}()
+	}
 }
 
 func GetStorage() Storage {
