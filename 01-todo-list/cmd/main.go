d0fb523cc2a76dead30319073dfa82efa4053883
Shamil <kasum543@gmail.com>
2025-01-04 14:56:03 +0300

    handling args

--- 01-todo-list/cmd/main.go
+++ 01-todo-list/cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"todo-list/internal/models"
+	"todo-list/internal/todo"
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
 
+	args := flag.Args()
+	models.CheckZeroArgs(args)
+
+	action := args[0]
+	actionFunc, ok := todo.ActionToFunc[action]
+	if !ok {
+		log.Fatalf(models.ErrorUnknownAction.Error(), action)
+	}
+
+	argsAfterAction := args[1:]
+	actionFunc(argsAfterAction)
 }
