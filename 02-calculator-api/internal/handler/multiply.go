fe26828a4655d01d80074fa2f06b5c229570a9b9
Shamil <kasum543@gmail.com>
2025-01-08 00:03:00 +0300

    multiply handler implemented

--- 02-calculator-api/internal/handler/multiply.go
+++ 02-calculator-api/internal/handler/multiply.go
@@ -1,10 +1,43 @@
 package handler
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+type multiplyRequest struct {
+	baseRequest
+}
+
+func (mr multiplyRequest) multiply() int {
+	return *mr.Number1 * *mr.Number2
+}
+
 func (h handler) Multiply(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "response for multiply\n")
+	defer r.Body.Close()
+
+	var requestBody multiplyRequest
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		http.Error(w, fmt.Sprintf("failed to decode request: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	// check input values
+	if err := requestBody.checkValues(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := baseResponse{
+		Result: requestBody.multiply(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, fmt.Sprintf("failed to encode response: %s", err), http.StatusInternalServerError)
+	}
+
 }
