24730f53073de32912bc79f38a6df82dc2da7a7c
Shamil <kasum543@gmail.com>
2025-01-08 00:30:23 +0300

    sum handler implemented

--- 02-calculator-api/internal/handler/sum.go
+++ 02-calculator-api/internal/handler/sum.go
@@ -1,10 +1,22 @@
 package handler
 
 import (
-	"io"
 	"net/http"
 )
 
+type sumRequest []int
+
+func (sr sumRequest) checkValues() error {
+	return nil
+}
+
+func (sr sumRequest) execute() (result int) {
+	for _, val := range sr {
+		result += val
+	}
+	return result
+}
+
 func (h handler) Sum(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "response for sum\n")
+	handleRequest(w, r, &sumRequest{})
 }
