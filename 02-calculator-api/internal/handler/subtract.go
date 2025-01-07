ba9b23ff00983e2953105810c406eb14815b272f
Shamil <kasum543@gmail.com>
2025-01-07 23:47:15 +0300

    subtract handler implemented

--- 02-calculator-api/internal/handler/subtract.go
+++ 02-calculator-api/internal/handler/subtract.go
@@ -1,10 +1,42 @@
 package handler
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+type subractRequest struct {
+	baseRequest
+}
+
+func (sr subractRequest) subtract() int {
+	return *sr.Number1 - *sr.Number2
+}
+
 func (h handler) Subtract(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "response for subtract\n")
+	defer r.Body.Close()
+
+	var requestBody subractRequest
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		http.Error(w, fmt.Sprintf("failed to decode request: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	// check input values
+	if err := requestBody.checkValues(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := baseResponse{
+		Result: requestBody.subtract(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, fmt.Sprintf("failed to encode response: %s", err), http.StatusInternalServerError)
+	}
 }
