fcef93b061a6bc3937a63d5371bb314ef8b15aa1
Shamil <kasum543@gmail.com>
2025-01-12 20:23:09 +0300

    router added

--- 04-url-shortener/cmd/main.go
+++ 04-url-shortener/cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+)
+
 func main() {
 
+	router := http.NewServeMux()
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	slog.Info("Starting website at localhost:8080")
+
+	err := srv.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error(fmt.Sprint("An error occured:", err))
+	}
 }
